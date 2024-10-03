b24f3c37d6f2ebbaec87ba69e3eb3f7771c40017
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2024-10-03 10:50:13 -0400

    add fallback for searchParams

--- packages/nextjs/app/artist-portfolio/page.tsx
+++ packages/nextjs/app/artist-portfolio/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import ArtistPortfolio from "./_components/ArtistPortfolio";
 import { NextPage } from "next";
@@ -8,17 +9,12 @@ const Portfolio: NextPage = () => {
   const searchParams = useSearchParams();
   const artistAddress = searchParams?.get("artistAddress");
 
-  if (!artistAddress) {
-    return (
-      <>
-        <div>loading ...</div>
-      </>
-    );
-  }
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-        <ArtistPortfolio artistAddress={artistAddress} />
+        <Suspense fallback={<div>Loading...</div>}>
+          {artistAddress ? <ArtistPortfolio artistAddress={artistAddress} /> : <div>Loading...</div>}
+        </Suspense>
       </div>
     </>
   );
