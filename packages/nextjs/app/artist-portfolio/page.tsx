625c0e0d770d9e468813e000cef332495bc0bff7
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2024-10-03 11:09:16 -0400

    fix: searchParams

--- packages/nextjs/app/artist-portfolio/page.tsx
+++ packages/nextjs/app/artist-portfolio/page.tsx
@@ -6,18 +6,22 @@ import ArtistPortfolio from "./_components/ArtistPortfolio";
 import { NextPage } from "next";
 
 const Portfolio: NextPage = () => {
-  const searchParams = useSearchParams();
-  const artistAddress = searchParams?.get("artistAddress");
-
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
         <Suspense fallback={<div>Loading...</div>}>
-          {artistAddress ? <ArtistPortfolio artistAddress={artistAddress} /> : <div>Loading...</div>}
+          <SearchParamsWrapper />
         </Suspense>
       </div>
     </>
   );
+};
+
+const SearchParamsWrapper = () => {
+  const searchParams = useSearchParams();
+  const artistAddress = searchParams?.get("artistAddress");
+
+  return artistAddress ? <ArtistPortfolio artistAddress={artistAddress} /> : <div>Loading...</div>;
 };
 
 export default Portfolio;
